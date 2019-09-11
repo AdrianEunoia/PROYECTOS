3fe2a66b3d678687afc65d2a2d4727bec42e65a9
Adrián <45033738+AdrianEunoia@users.noreply.github.com>
2019-09-11 17:59:37 +0200

    Update Entrada.java

--- src/Entrada.java
+++ src/Entrada.java
@@ -2,6 +2,6 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.printf("Primer proyecto de JAVA en GitHub!!");
         System.out.printf("Prueba");
-        System.out.printf("Pull desde arriba");
+        System.out.printf("Pull desde arriba!");
     }
 }
