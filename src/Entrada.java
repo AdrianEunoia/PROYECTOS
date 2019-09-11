228da4012abd436fb3fe8894b6143ae0bc3ca142
Adrián <adrian@certifyad.com>
2019-09-11 17:34:40 +0200

    Primer commit

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,5 +1,7 @@
 public class Entrada {
     public static void main(String[] args) {
-        System.out.printf("Primer proyecto de JAVA en GitHub");
+        System.out.printf("Primer proyecto de JAVA en GitHub!!");
+        System.out.printf("Prueba");
+
     }
 }
