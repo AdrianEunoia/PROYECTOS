809486f7933c30ac3f4c938ed2c1b041850df689
Adrián <adrian@certifyad.com>
2019-09-11 17:55:05 +0200

    Commit simple.

--- src/Entrada.java
+++ src/Entrada.java
@@ -2,6 +2,5 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.printf("Primer proyecto de JAVA en GitHub!!");
         System.out.printf("Prueba");
-
     }
 }
